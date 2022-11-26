040df3a55d40ee5f753eef2f2495493c96c3a70d
Raviraj-singh9316 <119124087+Raviraj-singh9316@users.noreply.github.com>
2022-11-26 21:33:20 +0530

    Add files via upload

---
+++ email slicer (2).py
@@ -0,0 +1,15 @@
+def email_sclicer(email):
+    """
+    seperates text before "@" and after "@"
+    """
+    i=email.index("@")
+    return  email[:i],email[i+1:]  #returns a tuple containing string before and after "@"
+
+email=input("Enter yout email: ")
+while True:
+    if "@" in email:
+        username,domain=email_sclicer(email)
+        print(f"username: {username} and domain: {domain.upper()}")
+        break
+    else:
+        print("Enter correct Email")
